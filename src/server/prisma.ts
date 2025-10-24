069d74cbbbe8118a1912c028130530d614b2bf30
Syscat <60985614+syscat29@users.noreply.github.com>
2025-10-24 13:08:15 -0400

    Update prisma.ts

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,4 +1,13 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const prisma = new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
+}
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
