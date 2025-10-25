99fd9152f20ea50c247cb3b7b56fe9385418c218
Syscat <60985614+syscat29@users.noreply.github.com>
2025-10-24 23:52:40 -0400

    Create actions.ts

---
+++ src/server/actions.ts
@@ -0,0 +1,43 @@
+'use server'
+
+import generateShortcode from '../lib/generateShortcode'
+import prisma from './prisma'
+
+export async function createShortUrl(url: string) {
+  try {
+    const shortcode = generateShortcode()
+
+    const payload = {
+      original_url: url,
+      slug: shortcode,
+    }
+
+    const shortUrl = await prisma.uRL.create({ data: payload })
+
+    console.table({
+      id: shortUrl.id,
+      original_url: shortUrl.original_url,
+      slug: shortUrl.slug,
+    })
+
+    return shortUrl
+  } catch (error) {
+    console.error('Error creating short URL:', error)
+    throw error
+  }
+}
+
+export async function getShortLink(slug: string): Promise<string> {
+  try {
+    const url = await prisma.uRL.findUnique({ where: { slug } })
+
+    if (!url) {
+      return ''
+    }
+
+    return url.original_url
+  } catch (error) {
+    console.error('Error getting short URL:', error)
+    throw error
+  }
+}
