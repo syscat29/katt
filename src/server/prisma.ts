4e6af0467acb5cd1abd77e1a5816d88d7704d8f3
Syscat <60985614+syscat29@users.noreply.github.com>
2025-10-26 15:06:21 -0400

    Update prisma.ts

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate())
 }
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
